2980c1976989f7f050219ca879a5573462855d6e
batgo6o <biz.mitko@gmail.com>
2020-12-19 13:29:44 +0200

    Finally fixed the camera issue

--- WebPortal/videofeed/camera.py
+++ WebPortal/videofeed/camera.py
@@ -1,14 +1,15 @@
 import cv2
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
+class Cam:
+    def get_frames():
+        video = cv2.VideoCapture(0)
 
-    def __del__(self):
-        self.video.release()        
+        while True:
+            ret, frame = video.read()
 
-    def get_frame(self):
-        ret, frame = self.video.read()
+            ret, jpeg = cv2.imencode('.jpg', frame)
 
-        ret, jpeg = cv2.imencode('.jpg', frame)
-
-        return jpeg.tobytes()
+            frame = jpeg.tostring()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    
+        del(video)
--- WebPortal/videofeed/routes.py
+++ WebPortal/videofeed/routes.py
@@ -2,11 +2,10 @@ from flask import Blueprint, redirect, url_for, render_template, flash
 
 from flask_login import current_user
 from werkzeug.wrappers import Response
-from WebPortal.videofeed.camera import VideoCamera
+import cv2
+from WebPortal.videofeed.camera import Cam
 
 main = Blueprint('main', __name__)
-
-video_stream = VideoCamera()
 
 @main.route('/')
 def home():
@@ -15,12 +14,19 @@ def home():
     else:
         return redirect(url_for('authentication.login'))
 
-def gen(stream):
-    while True:
-        frame = stream.get_frame()
-        yield (frame)
+# def get_frame():
+#     camera=cv2.VideoCapture(0) #this makes a web cam object
+
+#     while True:
+#         retval, im = camera.read()
+#         imgencode=cv2.imencode('.jpg',im)[1]
+#         stringData=imgencode.tobytes()
+#         yield (b'--frame\r\n'
+#             b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+    
+#     del(camera)
 
 @main.route('/video_feed')
 def video_feed():
-        return Response(gen(video_stream))
+    return Response(Cam.get_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
     
