3c4fb86ddb8fced28ae02576522b13060c24688d
thermalthrottle <biz.mitko@gmail.com>
2021-01-18 21:23:29 +0200

    Updated requirements

--- WebPortal/authentication/entities/validator.py
+++ WebPortal/authentication/entities/validator.py
@@ -1,18 +1,17 @@
-from flask.helpers import flash, url_for
-from flask.templating import render_template
-from flask_login.utils import login_user
-from werkzeug.utils import redirect
+from flask import flash, url_for, redirect, render_template
+from flask_login import login_user
 
 from WebPortal.authentication.forms import LoginForm
 from WebPortal.models import users
-from WebPortal import bcrypt
+from WebPortal import db, bcrypt
 
-def LoginValidator(loginForm):
-    FoundUser = users.query.filter_by(username = loginForm.username.data).first()
-    CheckHash = bcrypt.check_password_hash(FoundUser.password, loginForm.password.data)
-    if FoundUser and CheckHash:
-        login_user(FoundUser, remember=loginForm.submit.data)
-        flash("Logged in", 'Success')
-        return redirect(url_for('usercontrol.userinfo'))
-    else:        
-        flash("Login Unsuccessful", 'Failed')
+def FindUserAndLogin(loginForm):
+    if loginForm.validate_on_submit():
+        FoundUser = users.query.filter_by(username = loginForm.username.data).first()
+        CheckHash = bcrypt.check_password_hash(FoundUser.password, loginForm.password.data)
+        if FoundUser and CheckHash:
+            login_user(FoundUser, remember=loginForm.submit.data)
+            flash("Logged in", 'Success')
+            return redirect(url_for('usercontrol.userinfo'))
+        else:        
+            flash("Login Unsuccessful", 'Failed')
--- WebPortal/authentication/routes.py
+++ WebPortal/authentication/routes.py
@@ -1,10 +1,8 @@
 from flask import Blueprint, request, flash, redirect, render_template, url_for
 from flask_login import login_user, current_user, logout_user
 
-from WebPortal import db, bcrypt
-from WebPortal.models import users
 from WebPortal.authentication.forms import RegisterForm, LoginForm
-from WebPortal.authentication.entities.validator import LoginValidator
+from WebPortal.authentication.entities.validator import FindUserAndLogin
 from WebPortal.authentication.entities.registrator import RegisterUser, CheckIfUserIsAdmin
 
 authenticate = Blueprint('authentication', __name__)
@@ -14,10 +12,8 @@ def login():
     if current_user.is_authenticated:
         flash("Already Logged in", 'Failed')
         return redirect(url_for('main.home'))
-    else:
-        loginForm = LoginForm()
-        if loginForm.validate_on_submit():
-            LoginValidator(loginForm)
+    loginForm = LoginForm()
+    FindUserAndLogin(loginForm)
     return render_template('login.html', form = loginForm)
 
 @authenticate.route('/register', methods=['GET', 'POST'])
@@ -25,7 +21,6 @@ def register():
     if current_user.is_authenticated:
         if current_user.isAdmin == False:
             flash("Can't register, unless you're an admin", 'Failed')
-            return redirect(url_for('main.home'))
     registerForm = RegisterForm()
     RegisterUser(registerForm)
     return render_template('register.html', form = registerForm)
