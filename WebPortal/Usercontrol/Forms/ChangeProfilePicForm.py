4357b1ccef9ce806931734b116224465f2d78be9
batgo6o <biz.mitko@gmail.com>
2021-02-28 17:07:13 +0200

    Updated Requierements

--- WebPortal/Usercontrol/Forms/ChangeProfilePicForm.py
+++ WebPortal/Usercontrol/Forms/ChangeProfilePicForm.py
@@ -5,11 +5,10 @@ from WebPortal.models import users
 from flask_wtf import FlaskForm
 from flask_login import current_user
 from flask_wtf.file import FileField, FileAllowed
-from wtforms.validators import Length, Optional
 
 # Initializing the ChangeProfilePictureForm class
 # The class intializes a form that helps us change the username of the user
 class ChangeProfilePictureForm(FlaskForm):
     # Form Fileds
-    profilePicture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'], 'File Not Allowed')])
-    submit = SubmitField("Change Profile Picture")
+    profilePicture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg' 'png'], 'File Not Allowed')])
+    submit = SubmitField("Updaye Profile Picture")
