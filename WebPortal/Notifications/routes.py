e9b5e506d0c68decef001c606eaedf06c4daf183
batgo6o <biz.mitko@gmail.com>
2021-03-02 09:23:18 +0200

    Notifications refactoring

---
+++ WebPortal/Notifications/routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint
+
+from WebPortal import SocketIOClient
+from WebPortal.Notifications.Entities.Trigger_Armed_Unarmed import ArmTriggerClass
+
+Notifications = Blueprint('Notifications', __name__)
+
+# Handles the ArnSystem button using WebSockets
+@SocketIOClient.on('Toggle_Arm')
+def HandleTrigger():
+    ArmTriggerClass.HandleSystemArm()
+
+# returns the update worker for the Onesignal notification service
+@Notifications.route('/OneSignalSDKUpdaterWorker.js')
+def OneSignalSDKUpdateWorker():
+    return Notifications.send_static_file('js/OneSignal_Service_Worker/OneSignalSDKUpdaterWorker.js')
+
+# returns the service worker for the Onesignal notification service
+@Notifications.route('/OneSignalSDKWorker.js')
+def OneSignalSDKWorker():
+    return Notifications.send_static_file('js/OneSignal_Service_Worker/OneSignalSDKWorker.js')
